c14af65524ee491bf52729960ba376179ccd65a4
AntoineSpiteri <antoine.spiteri.e25576@mcast.edu.mt>
2023-12-30 20:45:36 +0100

    mars level done
    I added duststroms and collectable rocks for more points for the player

---
+++ Assets/Scirpts/Mars/DustStrom.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DustStrom : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Vector3 moveDirection;
+    public Terrain terrain; // Reference to the Terrain
+    public float changeDirectionInterval = 5f; // Time in seconds to change direction
+    public float heightAboveTerrain = 1f; // Height above the terrain
+
+    private float timeSinceLastDirectionChange;
+    private Vector3 terrainSize;
+
+    void Start()
+    {
+        if (terrain != null)
+        {
+            terrainSize = terrain.terrainData.size;
+        }
+        else
+        {
+            Debug.LogError("Terrain not assigned to DustStorm script.");
+            return;
+        }
+
+        SetInitialPositionAndDirection();
+    }
+
+    void Update()
+    {
+        // Move the dust storm
+        Vector3 newPosition = transform.position + moveDirection * moveSpeed * Time.deltaTime;
+        newPosition.y = terrain.SampleHeight(newPosition) - 0.1f; // Adjust Y position based on terrain height
+
+        transform.position = newPosition;
+
+        // Change direction at intervals or if it reaches the terrain bounds
+        timeSinceLastDirectionChange += Time.deltaTime;
+        if (timeSinceLastDirectionChange >= changeDirectionInterval || IsOutOfBounds())
+        {
+            SetRandomDirection();
+            timeSinceLastDirectionChange = 0;
+        }
+    }
+
+    private bool IsOutOfBounds()
+    {
+        return transform.position.x < 0 || transform.position.x > terrainSize.x
+            || transform.position.z < 0 || transform.position.z > terrainSize.z;
+    }
+
+    private void SetRandomDirection()
+    {
+        moveDirection = new Vector3(Random.Range(-1f, 1f), 0, Random.Range(-1f, 1f)).normalized;
+    }
+
+    private void SetInitialPositionAndDirection()
+    {
+        float randomX = Random.Range(0, terrainSize.x);
+        float randomZ = Random.Range(0, terrainSize.z);
+        float y = terrain.SampleHeight(new Vector3(randomX, 0, randomZ)) + heightAboveTerrain;
+
+        transform.position = new Vector3(randomX, y , randomZ);
+
+        SetRandomDirection();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // Apply hazard effect to the player
+            // Example: Reduce player health, slow down movement, etc.
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // Remove or reduce the hazard effect from the player
+            // Example: Restore movement speed, etc.
+        }
+    }
+}
---
+++ Assets/Scirpts/Mars/SpawnRocks.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnRocks : MonoBehaviour
+{
+
+     public int NumberoFRocks = 5000;
+    public int NumberofDuststroms = 100;
+
+
+    public GameObject rock;
+
+    public GameObject DustStorm;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Terrain terrain = GetComponent<Terrain>();
+
+        
+
+
+        PlaceRocks(terrain, rock, NumberoFRocks);
+
+        PlaceDustStrom(terrain, DustStorm, NumberofDuststroms);
+
+    }
+
+
+    //reason for this scirpt as painting the rocks wont allow the player to collect the rocks while this method does allow me
+
+    //I think just the limitation of the terrain desginer 
+
+    private void PlaceRocks(Terrain terrain, GameObject rock, int numberofrocks)
+    {
+        TerrainData terrainData = terrain.terrainData;
+        Vector3 terrainSize = terrainData.size;
+
+
+        for (int i = 0; i < numberofrocks; i++)
+        {
+            float x = Random.Range(0, terrainSize.x);
+            float z = Random.Range(0, terrainSize.z);
+            float y = terrain.SampleHeight(new Vector3(x, 0, z)) + terrain.transform.position.y;
+
+            Vector3 rockPosition = new Vector3(x, y, z) + terrain.transform.position;
+            Instantiate(rock, rockPosition, Quaternion.identity);
+        }
+    }
+
+
+
+    private void PlaceDustStrom(Terrain terrain, GameObject rock, int numberofrocks)
+    {
+        TerrainData terrainData = terrain.terrainData;
+        Vector3 terrainSize = terrainData.size;
+        rock.GetComponent<DustStrom>().terrain = terrain;
+
+        for (int i = 0; i < numberofrocks; i++)
+        {
+            float x = Random.Range(0, terrainSize.x);
+            float z = Random.Range(0, terrainSize.z);
+            float y = terrain.SampleHeight(new Vector3(x, 0, z)) + terrain.transform.position.y;
+
+            Vector3 rockPosition = new Vector3(x, y, z) + terrain.transform.position;
+            Instantiate(rock, rockPosition, Quaternion.identity);
+        }
+    }
+
+}
